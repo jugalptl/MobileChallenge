25e05d9c04560c12acbbd45b11dea1c21cc26d26
JugalPtl <jugal207@rediffmail.com>
2017-08-22 23:37:38 -0400

    Initial commit with error handling

--- CurrencyConversion/app/src/main/java/com/example/igroup/currencyconversion/MainActivity.java
+++ CurrencyConversion/app/src/main/java/com/example/igroup/currencyconversion/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.PersistableBundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -20,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -60,8 +62,6 @@ public class MainActivity extends AppCompatActivity {
         gridview =(GridView)findViewById(R.id.gridView) ;
         ITEM_LIST = new ArrayList<>();
 
-
-
         /*Check for Internet permission*/
 
         if(!isNetworkAvailable()) {
@@ -71,10 +71,14 @@ public class MainActivity extends AppCompatActivity {
                     fetchCurrencyRates();
 
         }
+     /*GridView WIth Converted values*/
+
         arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,ITEM_LIST);
         gridview.setAdapter(arrayAdapter);
         spn_currency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-     @Override
+
+
+            @Override
      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
          if(i>0){
@@ -85,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
              Log.e("Exception",e.toString());
          }
 
-        ITEM_LIST.add(ITEM_LIST.size(),String.valueOf(convertedRate)+spn_currency.getSelectedItem());
+        ITEM_LIST.add(ITEM_LIST.size(),edtxt_amount.getText().toString()+"EUR = "+String.valueOf(convertedRate)+spn_currency.getSelectedItem());
         arrayAdapter.notifyDataSetChanged();
 
      }
@@ -94,20 +98,13 @@ public class MainActivity extends AppCompatActivity {
      @Override
      public void onNothingSelected(AdapterView<?> adapterView) {
 
-        spn_currency.setSelection(0);
      }
  });
 
 
     }
 
-    @Override
-    protected void onResume() {
-        Toast.makeText(MainActivity.this,"Hi",Toast.LENGTH_SHORT).show();
-        super.onResume();
 
-
-    }
 
     @Override
     public Object onRetainCustomNonConfigurationInstance() {
@@ -127,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
+                    hashMapRates.clear();
                     JSONObject rates = response.getJSONObject("rates");
                    Iterator<?> keys = rates.keys();
                    while(keys.hasNext())
@@ -145,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("VOLLEY RESPONSE ERRROR",error.toString());
+                Log.e("VOLLEY RESPONSE ERROR",error.toString());
 
                 handleVolleyError(error);
 
@@ -160,27 +158,9 @@ public class MainActivity extends AppCompatActivity {
         VolleyErrorHandler errorHandler = new VolleyErrorHandler(MainActivity.this);
         String message = errorHandler.handleVolleyError(error);
         /** View to display Error */
-        LinearLayout errorLayout = (LinearLayout) findViewById(R.id.errorContainer);
-        TextView messageText2 = (TextView) findViewById(R.id.message2);
+        RelativeLayout errorLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+        Snackbar.make(errorLayout,message,Snackbar.LENGTH_LONG).show();
 
-        tryAgainButton = (Button) findViewById(R.id.tryAgainButton);
-        /** make it visible */
-        errorLayout.setVisibility(View.VISIBLE);
-        messageText2.setVisibility(View.VISIBLE);
-        tryAgainButton.setVisibility(View.VISIBLE);
-        edtxt_amount.setVisibility(View.GONE);
-        spn_currency.setVisibility(View.GONE);
-        gridview.setVisibility(View.GONE);
-
-        messageText2.setText(message);
-        tryAgainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /** Load Activity Again */
-                finish();
-                startActivity(getIntent());
-            }
-        });
     }
 
     private void setSpnner() {
